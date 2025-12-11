c72dd88fcff9db9c04eb63a256a690be98ae8bd7
Rushhav <rushav182@gmail.com>
2025-12-11 21:06:08 +0530

    install mysql through shellscript

--- 10-install-script.sh
+++ 10-install-script.sh
@@ -1,30 +1,10 @@
 #!/bin/bash
 
-#need to check if the user is root user or not
 USERID=$(id -u)
 if [ $USERID -ne 0 ]
 then
-    echo "error:: User must have sudo access to execute this script"
-    exit1
+    echo "Error:: you must have sudo access to execute this script"
 fi
 
-#install mysql
-dnf install mysql -y
-
-if [ $? -ne 0 ]
-then
-    echo "Installing mysql..... Failure"
-    exit1
-else
-    echo "Installing mysql..... Success"
-fi
-
-#installing git
-dnf install git -y
-
-if [ $? -ne 0 ]
-then
-    echo "Installing git..... Failure"
-else
-    echo "Installing git..... Success"
-fi
+#need to install mysql
+dnf install mysqll -y
