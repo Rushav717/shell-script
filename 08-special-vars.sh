f3e800ff9c8d15af2329d34812e8236975a7c4f2
Rushhav <rushav182@gmail.com>
2025-12-11 19:39:01 +0530

    printing list of array values

--- 08-special-vars.sh
+++ 08-special-vars.sh
@@ -8,4 +8,4 @@ echo "home directory of current user: $HOME"
 echo "ehich user is running this script: $USER" 
 echo "process id of current script: $$"
 sleep 60 &
-echo "process id of last command in background: $!"
+echo "process is of last command in background: $!"
