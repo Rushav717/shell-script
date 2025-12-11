f304748ffcaaf611f407a17be9da94308191fc50
Rushhav <rushav182@gmail.com>
2025-12-11 19:53:12 +0530

    if else condition for number greater or less than 100

---
+++ 09-conditions.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+NUMBER=$!
+
+if [$NUMBER -gt 100]   #-gt , -lt, -eq, -ge, -le
+
+then
+    echo "given number is greater than 100"
+
+else
+    echo "given number is less then or equal to 100"
+
+fi
