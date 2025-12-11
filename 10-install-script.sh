2e6bceaaa62f7737e040a86e9efee52e373e42e9
Rushhav <rushav182@gmail.com>
2025-12-11 20:01:41 +0530

    install mysql through shellscript

---
+++ 10-install-script.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+#need to check if the user is root user or not
+USERID=$(id -u)
+if [ USERID -ne 0 ]
+then
+    echo "error:: User must have sudo access to execute this script"
+fi
+
+#install mysql
+dnf install mysql -y
