5d7354afcfd3cfa2f8f60610e3dfefd97d8cb44c
Rushhav <rushav182@gmail.com>
2025-12-13 09:35:10 +0530

    installing mysql and git using Functions

---
+++ 11-functions.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+USERID=$(id -u)
+
+VALIDATE()
+{
+    if [ $1 -ne 0 ]
+    then
+        echo "$2 installing ..... Failure"
+    else
+        echo "$2 installing...... Success"
+    fi
+}
+
+if [ USERID -ne 0 ]
+then
+    echo "error:: Sudo access required"
+    exit 1
+fi
+
+dnf list installed mysql
+if [ $? -ne 0 ]
+then
+    dnf install mysql -y
+    VALIDATE $? mysql
+else
+    echo "mysql is already....... installed"
+fi
+
+dnf list installed git
+if [ $? -ne 0 ]
+then
+    dnf install git -y
+    VALIDATE $? git
+else
+    echo "git is already........ installed"
+fi
