ff7633fc72385d09cec366075e3e0b7222a46c1f
Rushhav <rushav182@gmail.com>
2025-12-14 10:30:43 +0530

    creating logs

--- 13-logs.sh
+++ 13-logs.sh
@@ -5,7 +5,10 @@ R="\e[31m"
 G="\e[32m"
 Y="\e[33m"
 N="\e[0m"
+
 LOGS_FOLDER="/var/log/shellscript_logs"
+mkdir -p $LOGS_FOLDER
+
 LOG_FILE=$(echo $0 | cut -d "." -f1)
 TIMESTAMP=$(date +%Y-%m-%d-%H-%M-%S)
 LOG_FILE_NAME="$LOGS_FOLDER/$LOG_FILE-$TIMESTAMP.log"
@@ -14,9 +17,9 @@ VALIDATE()
 {
 if [ $1 -ne 0 ]
 then
-    echo -e "$2 is......$R Failure"
+    echo -e "$2 is......$R Failure $N"
 else
-    echo -e "$2 is......$G Success"
+    echo -e "$2 is......$G Success $N"
 fi
 }
 
@@ -34,7 +37,7 @@ then
     dnf install mysql -y &>> $LOG_FILE_NAME
     VALIDATE $? "Installing MYSQL"
 else
-    echo -e "mysql is already...... $Y installed"
+    echo -e "mysql is already...... $Y installed $N"
 fi
 
 dnf list installed git &>> $LOG_FILE_NAME
@@ -43,5 +46,5 @@ then
     dnf install git -y &>> $LOG_FILE_NAME
     VALIDATE $? "Installing git"
 else
-    echo -e "git is already.......... $Y installed "
+    echo -e "git is already.......... $Y installed $N "
 fi
