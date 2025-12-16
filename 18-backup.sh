dcddd478e2e5c6e54b42d9d85cc4a152ad7db9d3
Rushhav <rushav182@gmail.com>
2025-12-16 09:09:42 +0530

    backup-script

--- 18-backup.sh
+++ 18-backup.sh
@@ -10,6 +10,8 @@ DESTINATION_DIR=$2
 DAYS=${3:-14}
 
 LOGS_FOLDER="/home/ec2-user/shellscript-logs"
+mkdir -p /home/ec2-user/shell-script-logs/
+
 LOG_FILE=$(echo $0 | cut -d "." -f1)
 TIMESTAMP=$(date +%Y-%m-%d-%H-%M-%S)
 LOG_FILE_NAME="$LOGS_FOLDER/$LOG_FILE-$TIMESTAMP.log"
@@ -18,8 +20,6 @@ USAGE(){
     echo -e "$R USAGE:: $N sh 18-backup.sh <SOURCE_DIR> <DESTINATION_DIR> <DAYS(OPTIONAL)>"
     exit 1
 }
-
-mkdir -p /home/ec2-user/shell-script-logs/
 
 if [ $# -lt 2 ]
 then
