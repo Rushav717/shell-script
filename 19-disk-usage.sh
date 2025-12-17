0f3e7d2633ee236fccaeb0bd603ebacfa2c22cf3
Rushhav <rushav182@gmail.com>
2025-12-17 20:25:30 +0530

    diskusage-script

--- 19-disk-usage.sh
+++ 19-disk-usage.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 
 DISK_USAGE=$(df -hT | grep xfs)
+DISK_THRESHOLD=5
 
 while read -r line
 do
-  echo $line
+  USAGE=$(echo $line | cut -d " " -f6)
+  PARTITION=$(echo $line | cut -d " " -nf)
+  echo "PARTITION=$PARTITION and USAGE=$USAGE"
 done <<< $DISK_USAGE
