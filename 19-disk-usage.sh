9df11fa1200ebf4f2075bfea1e79bc80018b00a6
Rushhav <rushav182@gmail.com>
2025-12-17 20:35:23 +0530

    diskusage-script

--- 19-disk-usage.sh
+++ 19-disk-usage.sh
@@ -2,10 +2,16 @@
 
 DISK_USAGE=$(df -hT | grep xfs)
 DISK_THRESHOLD=5
-
+msg=""
 while read -r line
 do
   USAGE=$(echo $line | awk -F " " '{print $6F}' | cut -d "%" -f1 )
   PARTITION=$(echo $line | awk -F " " '{print $NF}')
   echo "PARTITION=$PARTITION and USAGE=$USAGE"
+  if [ $USAGE -ge $DISK_THRESHOLD ]
+  then
+    msg="high disk usage on partition: $PARTITION usage is: $USAGE"
+  fi
 done <<< $DISK_USAGE
+
+echo "message:$msg"
