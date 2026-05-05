425eb095fa1166dce52553937065a65f09a5665f
Rushhav <rushav182@gmail.com>
2026-05-05 17:43:00 +0530

    colors

---
+++ 12-colors.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+USERID=$(id -u) #for checking root user or not
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+
+VALIDATE() { 
+    if [ $1 -ne 0 ]
+    then 
+        echo -e "$2 ... $R failure"
+        exit 1
+    else 
+        echo -e "$2 ... $G success"
+    fi
+}
+
+if [ $USERID -ne 0 ]
+then
+    echo "ERROR:: you must have sudo access to execute the script"
+    exit 1
+fi
+
+dnf list installed mysql
+if [ $? -ne 0 ]
+then 
+    dnf install mysql -y
+    VALIDATE $? "Installing mysql"
+else
+    echo -e "Mysql is already .... $Y installed"
+fi
+
+dnf list installed git
+if [ $? -ne 0 ]
+then
+    dnf install git -y
+    VALIDATE $? "Installing git"
+else
+    echo "Git is already ..... $Y installed"
+fi
