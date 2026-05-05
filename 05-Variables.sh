1b5c1ec828db1ce61bfb446ae055ad30ad972688
Rushhav <rushav182@gmail.com>
2026-05-05 10:50:15 +0530

    USERNAME AND PWD should be printed but not visible

--- 05-Variables.sh
+++ 05-Variables.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 echo "please enter username::"
-read -S USERNAME
+read -s USERNAME
 echo "username entered: $USERNAME"
 echo "Please enter your password::"
-read -S PASSWORD
+read -s PASSWORD
