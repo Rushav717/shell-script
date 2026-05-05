8f25403efb3e3de3e19e8354a3fb6dd2ccf07626
Rushhav <rushav182@gmail.com>
2026-05-05 16:21:29 +0530

    installing mysql script

---
+++ 10-install-script.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+#need to check if the user running the script is root user or not
+
+USERID=$(id -u)
+if [ USERID -ne 0 ]
+then
+    echo "must have sudo access to execute the script"
+fi
+
+dnf install mysqll -y
