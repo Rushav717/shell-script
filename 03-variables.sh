4f7663654406179cae0502de155b9365d9cdb21b
Rushhav <rushav182@gmail.com>
2026-05-05 10:39:19 +0530

    declaring variables

---
+++ 03-variables.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+PERSON1=Ramesh
+PERSON2=Suresh
+
+echo "$PERSON1:: Hi $PERSON2"
+echo "$PERSON2:: Hi $PERSON1"
+echo "$PERSON1:: How are you doing?"
+echo "$PERSON2:: I am doing good, Thanks for asking ramesh!!!"
+
