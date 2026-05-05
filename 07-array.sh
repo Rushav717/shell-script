35dd297f438b66864a07d1756d9d7c5e26f6da7a
Rushhav <rushav182@gmail.com>
2026-05-05 11:10:28 +0530

    printing list of values for movies

---
+++ 07-array.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+Movies=("pushpa" "rrr" "kgf")
+
+echo "first movie: ${Movies[0]}"
+echo "Second movie: ${Movies[1]}"
+echo "Third movie: ${Movies[2]}"
+echo "All Movies: ${Movies[@]}"
