d20a6c09f182725efee6ade3b20841e12c8bdc99
Rushhav <rushav182@gmail.com>
2026-05-06 05:54:19 +0530

    install packages

---
+++ 15-install-packages.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+
+USERID=$(id -u)
+
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+
+LOGS_FOLDER="/var/log/shellscript-logs"
+mkdir -p $LOGS_FOLDER #folder will be created if not created
+LOG_FILE=$(echo $0 | cut -d "." -f1)  #13-logs
+TIMESTAMP=$(date +%Y-%m-%d-%H-%M-%S)
+LOG_FILE_NAME="$LOGS_FOLDER/$LOG_FILE-$TIMESTAMP.log"
+
+VALIDATE() { 
+    if [ $1 -ne 0 ]
+    then 
+        echo -e "$2 ... $R failure"
+        exit 1
+    else 
+        echo -e "$2 ... $G success"
+    fi
+}
+
+echo "Script started executing at: $TIMESTAMP" &>> $LOG_FILE_NAME
+
+if [ $USERID -ne 0 ]
+then
+    echo "ERROR:: you must have sudo access to execute the script"
+    exit 1
+fi
+
+for package in $@
+do
+    dnf list installed $package &>> $LOG_FILE_NAME
+    if [ $? -ne 0 ]
+    then
+        dnf install $package -y &>> $LOG_FILE_NAME
+        VALIDATE $? "Installing $package"
+    else
+        echo -e "$package is already ..... $Y installed $N"
+done
